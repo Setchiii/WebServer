bfddb718062b64f0381469d0498e5e374fc60b26
Setchiii <foccroullemax@gmail.com>
2024-02-11 22:42:01 +0100

    Website working

--- wsgi.py
+++ wsgi.py
@@ -4,11 +4,38 @@ import os
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    return render_template("home.html")
-
-
 @app.route("/favicon.ico")
 def favico():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'assets/favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route("/index.html")
+def home():
+    return render_template("index.html")
+
+@app.route("/")
+def home2():
+    return render_template("index.html")
+
+@app.route("/works.html")
+def works():
+    return render_template("works.html")
+
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact.html")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/work.html")
+def work():
+    return render_template("work.html")
+
+@app.route("/components.html")
+def components():
+    return render_template("components.html")
+
+@app.route("/thankyou.html")
+def thankyou():
+    return render_template("thankyou.html")
